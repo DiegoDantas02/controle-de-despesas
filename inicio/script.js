2a1a049c5efd50b6005ce81899f1321ca67725a9
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-06-15 17:52:33 -0300

    .

--- inicio/script.js
+++ inicio/script.js
@@ -1,12 +1,31 @@
+const transactionsUl = document.querySelector('#transactions');
+
 const dummyTransactions = [
     { id: 1, name: 'Bolo de brigadeiro', amount: -20 },
     { id: 2, name: 'Salário', amount: 300 },
     { id: 3, name: 'Torta de frango', amount: -10 },
     { id: 4, name: 'Violão', amount: 150 }
-]
+];
 
 const addTransactionIntoDOM = transaction => {
-    <li class="minus">
-        Salário <span>-$400</span><button class="delete-btn">x</button>
-    </li>
+    const operator = transaction.amount < 0 ? '-' : '+';
+    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus';
+    const amountWithoutOperator = Math.abs(transaction.amount);
+    const li = document.createElement('li');
+
+    li.classList.add(CSSClass);
+    li.innerHTML = `
+        ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}</span><button class="delete-btn">x</button>
+    `;
+
+    transactionsUl.append(li);
+};
+
+const updateBalanceValues = () => {
+    const transactionAmounts = dummyTransactions.map(transactions => transaction.amount)
 }
+const init = () => {
+    dummyTransactions.forEach(addTransactionIntoDOM)
+    updateBalanceValues()
+}
+init()
