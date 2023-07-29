3e72efadd9d270c9a843e2331ac20ef5ea1c800e
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-07-29 18:26:14 -0300

    .

--- tela-bloqueio/validar.php
+++ tela-bloqueio/validar.php
@@ -1,3 +1,10 @@
 <?php
-    header('Location: ../inicio/home.php');
+$usuario = $_POST["login"];
+$senha = $_POST["password"];
+
+if($usuario == "diego@wolf.com" && $senha == 1234){
+    header("location:home.php");
+}else{
+    header("location: login.php");
+}
 ?>
